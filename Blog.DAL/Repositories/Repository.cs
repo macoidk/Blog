71466dc94eb99447659ff00d6fba96be8207bf93
fight <fightm45@gmail.com>
2025-03-21 00:33:11 +0200

    change using change implementation of crud operations due to new generic repository

--- Blog.DAL/Repositories/Repository.cs
+++ Blog.DAL/Repositories/Repository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 using BlogSystem.DAL.Context;
+using BlogSystem.Abstractions;
 using Microsoft.EntityFrameworkCore;
 
 namespace BlogSystem.DAL.Repositories
@@ -12,38 +13,58 @@ namespace BlogSystem.DAL.Repositories
     {
         protected readonly BlogDbContext Context;
         protected readonly DbSet<TEntity> DbSet;
-        
+
         public Repository(BlogDbContext context)
         {
             Context = context;
             DbSet = context.Set<TEntity>();
         }
-        
+
         public async Task<TEntity> GetByIdAsync(int id)
         {
             return await DbSet.FindAsync(id);
         }
-        
+
         public async Task<IEnumerable<TEntity>> GetAllAsync()
         {
             return await DbSet.ToListAsync();
         }
-        
+
         public async Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate)
         {
             return await DbSet.Where(predicate).ToListAsync();
         }
-        
+
+        public async Task<TEntity> GetByIdWithIncludesAsync(int id, params Expression<Func<TEntity, object>>[] includes)
+        {
+            IQueryable<TEntity> query = DbSet;
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+            return await query.FirstOrDefaultAsync(e => EF.Property<int>(e, "Id") == id);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllWithIncludesAsync(params Expression<Func<TEntity, object>>[] includes)
+        {
+            IQueryable<TEntity> query = DbSet;
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+            return await query.ToListAsync();
+        }
+
         public async Task AddAsync(TEntity entity)
         {
             await DbSet.AddAsync(entity);
         }
-        
+
         public void Update(TEntity entity)
         {
             DbSet.Update(entity);
         }
-        
+
         public void Delete(TEntity entity)
         {
             DbSet.Remove(entity);
--- Blog.DAL/Repositories/TagRepository/TagRepository.cs
+++ Blog.DAL/Repositories/TagRepository/TagRepository.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using BlogSystem.DAL.Context;
-using BlogSystem.DAL.Entities;
+using BlogSystem.Models;
+using BlogSystem.Abstractions;
 using Microsoft.EntityFrameworkCore;
 
 namespace BlogSystem.DAL.Repositories
@@ -12,7 +12,7 @@ namespace BlogSystem.DAL.Repositories
         public TagRepository(BlogDbContext context) : base(context)
         {
         }
-        
+
         public async Task<IEnumerable<Tag>> GetByArticleIdAsync(int articleId)
         {
             return await Context.ArticleTags
@@ -20,11 +20,10 @@ namespace BlogSystem.DAL.Repositories
                 .Select(at => at.Tag)
                 .ToListAsync();
         }
-        
+
         public async Task<Tag> GetByNameAsync(string name)
         {
-            return await Context.Tags
-                .FirstOrDefaultAsync(t => t.Name == name);
+            return await FindAsync(t => t.Name == name).ContinueWith(t => t.Result.FirstOrDefault());
         }
     }
 }
